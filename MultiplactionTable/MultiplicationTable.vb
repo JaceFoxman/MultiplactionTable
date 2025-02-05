2e6ecdcba876c754a59825a89b345f7258b3ee95
Jason Permann <JaceFoxman@gmail.com>
2025-02-04 18:26:30 -0700

    Add 12 times table

--- MultiplactionTable/MultiplicationTable.vb
+++ MultiplactionTable/MultiplicationTable.vb
@@ -3,9 +3,33 @@
 'RCET2265
 'Multiplication Table
 'https://github.com/JaceFoxman/MultiplactionTable.git
+
+Option Strict On
+Option Explicit On
+Option Compare Text
 Module MultiplicationTable
 
     Sub Main()
+
+
+    End Sub
+
+    Sub FirstTable()
+
+        Dim result As String
+
+        For i = 1 To 12
+
+            For j = 1 To 12
+                result = (i * j).ToString
+                result = result.PadLeft(4)
+                Console.Write(result)
+            Next
+
+            Console.WriteLine()
+
+        Next
+
 
     End Sub
 
