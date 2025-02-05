a7cfe5bf2ab00bf1bfa977d17215e499e67a70ba
Jason Permann <JaceFoxman@gmail.com>
2025-02-04 19:15:41 -0700

    Create Menu

--- MultiplactionTable/MultiplicationTable.vb
+++ MultiplactionTable/MultiplicationTable.vb
@@ -7,14 +7,44 @@
 Option Strict On
 Option Explicit On
 Option Compare Text
+
 Module MultiplicationTable
 
     Sub Main()
 
+        Dim userinput As String
+        Dim variable As Integer
+
+        'Boot up menu
+        Console.WriteLine("Please enter a number. Enter " & "Q " & "at any time to quit")
+        Console.WriteLine()
+
+        Do Until userinput = "Q"
+
+            Try
+                Console.WriteLine("Choose a number: ")
+                userinput = Console.ReadLine()
+                variable = CInt(userinput)
+                Console.WriteLine($"You entered {variable}")
+                Console.WriteLine()
+
+                Console.WriteLine(StrDup(80, "-"))
+                Console.WriteLine($"Enjoy Your {userinput} X {userinput} Times Table:")
+                MultiplacationTable()
+            Catch ex As Exception
+
+                If userinput <> "Q" Then Console.WriteLine("Please enter a whole number")
+
+            End Try
+
+        Loop
+
+        Console.WriteLine("Have a nice day")
+        Console.WriteLine("Press enter to close the window")
 
     End Sub
 
-    Sub FirstTable()
+    Sub MultiplacationTable()
 
         Dim result As String
 
@@ -30,23 +60,7 @@ Module MultiplicationTable
 
         Next
 
-    End Sub
-
-    Sub SecondTable()
-
-        Dim result As String
-
-        For i = 1 To 7
-
-            For j = 1 To 7
-                result = (i * j).ToString
-                result = result.PadLeft(4)
-                Console.Write(result)
-            Next
-
-            Console.WriteLine()
-
-        Next
+        Console.WriteLine()
 
     End Sub
 
